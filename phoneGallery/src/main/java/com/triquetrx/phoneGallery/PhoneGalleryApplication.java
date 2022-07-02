088d69ce5cf97b2c2eeebf6e1ba08452352a6c12
Khan <2146122@cognizant.com>
2022-07-02 17:24:17 +0530

    New Project

---
+++ phoneGallery/src/main/java/com/triquetrx/phoneGallery/PhoneGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.triquetrx.phoneGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneGalleryApplication.class, args);
+	}
+
+}
---
+++ phoneGallery/src/test/java/com/triquetrx/phoneGallery/PhoneGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triquetrx.phoneGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
